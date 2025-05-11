ae13bb957e90b4db036eedf39e603d9e16a39ade
ionutsandu1996 <ionutdanielsandu96@gmail.com>
2025-05-12 00:16:04 +0300

    add CategoryCard and App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { BrowserRouter as Route, Switch } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Footer/>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+        {/* Adăugați mai multe rute pentru paginile de categorie sau video */}
+      </Switch>
+    </Router>
   );
 }
 
